ce41f25fe36ff84a6c8de83b86dd81ef784a536a
tenmark86e <tenmark86@gmail.com>
2022-12-03 16:52:53 +0300

    defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'hero_powers/index'
+  # resources :hero_powers
+  # resources :powers
+  # resources :heros
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :heros ,only:[:index,:show,]
+  resources :powers ,only:[:index,:show,:update,:create]
+  get '/hero_powers', to: 'hero_powers#index'
+  post '/hero_powers', to: 'hero_powers#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'seed'
+
+10.times do
+    Power.create(
+       name: Faker::Superhero.power,
+       description: Faker::TvShows::BigBangTheory.quote
+    )
+end
+
+10.times do
+    HeroPower.create(
+       strength: ['Weak', 'Average','Strong'].sample,
+       hero_id: rand(1..5),
+       power_id: rand(1..10)
+    )
+end
+
+5.times do
+    Hero.create(
+       name: Faker::Superhero.name,
+        super_name: Faker::Superhero.descriptor
+    )
+end
+
+puts 'end'
